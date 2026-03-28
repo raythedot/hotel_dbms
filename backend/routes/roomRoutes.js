e6b2f2cbf9bf715d9d70bbcca8fc87c786abc907
raythedot <shivamray15@gmail.com>
2026-03-28 13:07:18 +0530

    3rd

---
+++ backend/routes/roomRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const roomController = require('../controllers/roomController');
+
+router.get('/', roomController.getAllRooms);
+router.post('/', roomController.addRoom);
+router.put('/:id', roomController.updateRoom);
+router.delete('/:id', roomController.deleteRoom);
+
+module.exports = router;
